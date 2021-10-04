d4439c8e6219fa846e5cbf3242f51ddd0661a0e1
Robin Hemmingsson <hemmingsson.robin@gmail.com>
2021-10-04 14:52:17 +0200

    removed unnessary classes and imports

--- server/src/main/java/com/example/demo/Controllers/Controller.java
+++ server/src/main/java/com/example/demo/Controllers/Controller.java
@@ -1,17 +1,12 @@
 package com.example.demo.Controllers;
-
 import com.example.demo.Compute.Parser;
 import com.example.demo.DTO.Dto;
-import com.example.demo.DTO.Response;
-import org.junit.validator.ValidateWith;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 import java.io.IOException;
-import java.util.ArrayList;
+
 import java.util.List;
 
 @RestController
